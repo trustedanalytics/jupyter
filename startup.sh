907558e58d33bba451840b5c23dc260d570cd548
ashahba <Abolfazl.Shahbazi@intel.com>
2016-05-04 11:15:22 -0700

    Fix the Config Dir paths

--- startup.sh
+++ startup.sh
@@ -7,9 +7,12 @@ pushd ${CDH_CONFIG_DIR}
 if [ ! -z "$SPARK_ON_YARN_CLIENT_CONFIG" ]; then
 	curl ${SPARK_ON_YARN_CLIENT_CONFIG} > spark.zip
 	unzip spark.zip
-	cp spark-conf/* -p $SPARK_CONF_DIR
-	cp hadoop-conf -p $HADOOP_CONF_DIR
+	mkdir -p $SPARK_CONF_DIR
+	mkdir -p $HADOOP_CONF_DIR
+	cp spark-conf/* $SPARK_CONF_DIR
+	cp hadoop-conf $HADOOP_CONF_DIR
 fi;
 popd
 
+unset CDH_CONFIG_DIR
 source /usr/local/bin/start-notebook.sh
