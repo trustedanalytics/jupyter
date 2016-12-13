c59a6c54b8ec1d139edad798a9c9ed7ca694e0f3
ashahba <Abolfazl.Shahbazi@intel.com>
2016-12-13 00:20:04 -0500

    git push only if there are local changes

--- updateDockerfile.sh
+++ updateDockerfile.sh
@@ -9,11 +9,17 @@ sed -i "s|ARG TKLIBS_INSTALLER_URL=.*|ARG TKLIBS_INSTALLER_URL=\"$DAALTK_URL\"|g
 
 git clone git@github.com:trustedanalytics/jupyter.git -b $BRANCH $BRANCH
 cp -rp Dockerfile $BRANCH
+
 pushd $BRANCH
 git status
-git diff Dockerfile
-git add Dockerfile
-git commit -m "update daaltk url" Dockerfile 
-echo git push origin $BRANCH
-git push origin $BRANCH
+if [ -n "$(git status --porcelain)" ]; then 
+  echo "local changes need to be pushed"
+  git add Dockerfile
+  git commit -m "update daaltk url" Dockerfile 
+  echo git push origin $BRANCH
+  git push origin $BRANCH
+else 
+  echo "no changes";
+fi
+
 popd
