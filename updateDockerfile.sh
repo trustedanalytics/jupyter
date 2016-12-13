2df2b547f526cce7229c6c4450c7ce24ce3c9704
ashahba <Abolfazl.Shahbazi@intel.com>
2016-12-12 21:27:40 -0500

    Modify update script so that it works off a fresh clone:wq

--- updateDockerfile.sh
+++ updateDockerfile.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 BRANCH=$(echo $BRANCH | sed -e "s|tags/||g")
+DOCKER_UPDATE_BRANCH="docker-update"
+
 chmod +x git-asset
 
 DAALTK_URL=$(./git-asset -t $GIT_TOKEN -o trustedanalytics -r daal-tk download  --artifact-type daaltk-installer --release-type RC)
 echo $DAALTK_URL
-
-
-echo $DAALTK_URL
 sed -i "s|ARG TKLIBS_INSTALLER_URL=.*|ARG TKLIBS_INSTALLER_URL=\"$DAALTK_URL\"|g" Dockerfile
+
+git clone git@github.com:trustedanalytics/jupyter.git -b $DOCKER_UPDATE_BRANCH $DOCKER_UPDATE_BRANCH
+cp -rp Dockerfile $DOCKER_UPDATE_BRANCH
+pushd $DOCKER_UPDATE_BRANCH
 git status
-
 git diff Dockerfile
-
-git commit -m "update sparktk build" Dockerfile 
-
-
-if [ "$BRANCH" != "" ]; then
-
-  echo git checkout -b $BRANCH
-  git checkout -b $BRANCH 
-
-  echo git push origin $BRANCH
-  git push origin $BRANCH
-fi
+git add Dockerfile
+git commit -m "update daaltk url" Dockerfile 
+echo git push origin $BRANCH
+git push origin $BRANCH
+popd
